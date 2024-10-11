63edaba2e207814533089710085eae2c01b58571
Space Pocket <space1985pocket@gmail.com>
2024-10-11 15:41:50 +0300

    refactor: update error msgs

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -18,14 +18,27 @@ const useHttp = (): UseHttpType => {
       try {
         const response = await fetch(url, options);
         if (!response.ok) {
-          throw new Error(
-            `We have an error in fetching data. ${response.statusText} Code status - ${response.status}`
-          );
+          const errorData = await response.json();
+
+          if (errorData.errors && Array.isArray(errorData.errors)) {
+            const errorMessages = errorData.errors
+              .map((err: { msg: unknown }) => err.msg)
+              .join(', ');
+            throw new Error(
+              `Errors: ${errorMessages}. Code status - ${response.status}`
+            );
+          } else {
+            const errorMessage = errorData.message || response.statusText;
+            throw new Error(
+              `Error: ${errorMessage}. Code status - ${response.status}`
+            );
+          }
         }
 
         const data: T = await response.json();
         return data;
       } catch (error) {
+        console.log('er => ', error);
         if (error instanceof Error) setIsError(error.message);
       } finally {
         setIsLoading(false);
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import { Wrapper } from '../components/wrapper/Wrapper';
+import { RoutePaths } from '../routes/routePaths';
 
 export const NotFound: React.FC = () => {
   return (
     <Wrapper title={'NotFound Page'}>
-      <div></div>
+      <Link to={RoutePaths.MainPage}>Go to main</Link>
     </Wrapper>
   );
 };
